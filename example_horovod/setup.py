191747b5f481d93c6be45748c50fe6dd009e2330
ATC24-FGBuff <zqming@hust.edu.cn>
2024-01-17 16:39:12 +0800

    update

--- example_horovod/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='adtopk_lib',
-      version='1.0',
-      description='All-Dimension TopK Distributed Trainning Framework',
-      author= 'Zhangqiang Ming',
-      packages=find_packages()
-    )
